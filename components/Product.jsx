1b61729436528f14e3f716d31106fee168a65a9c
ebaykalgurbuz <ebaykalgurbuz@gmail.com>
2023-03-18 18:24:35 +0300

    Product component completed also footer

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
-const Product = () => {
+import Link from 'next/link'
+import { urlFor } from '@/lib/client'
+const Product = ({product:{image,name,slug,price}}) => {
   return (
-    <div>Product</div>
+    <div>
+      <Link href={`/product/${slug.current}`}></Link>
+      <div className='product-cart'>
+      <img src={urlFor(image[0])} width={250} height={250} className="product-image" alt="" />
+      <p className="product-name">{name}</p>
+      <p className="product-name">${price}</p>
+      </div>
+    </div>
   )
 }
 
