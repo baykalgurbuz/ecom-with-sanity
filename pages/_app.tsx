ccb3c989f32845d49d782577e80530206273b463
ebaykalgurbuz <ebaykalgurbuz@gmail.com>
2023-03-20 16:13:24 +0300

    StateContex added to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import React from 'react'
-import {Layout} from '../components'
+import "@/styles/globals.css";
+import {Toaster} from 'react-hot-toast';
+import type { AppProps } from "next/app";
+import React from "react";
+import { Layout } from "../components";
+import { StateContext } from "../context/StateContext";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-  <Component {...pageProps} />
-  </Layout>
-  )
+    <StateContext>
+      <Layout>
+        <Toaster/>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
